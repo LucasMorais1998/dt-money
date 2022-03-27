b1f011ad00ef0b0f2ee8c51ed2bdec83b90338bd
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-03-26 23:19:14 -0300

    creating radiobox, styling and adding functionality

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,9 +2,9 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --red: #E62E4D;
     --blue: #5429CC;
     --green: #33CC95;
+    --red: #E62E4D;
     
     --blue-light: #6933FF;
     
